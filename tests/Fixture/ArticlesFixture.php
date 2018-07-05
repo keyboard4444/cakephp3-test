e85fb4ccb1a6aaa11a57f2381939dda1f537d0b3
SIF <keyboard4444@gmail.com>
2018-07-05 22:12:34 +0800

    learn cakephp unit test
    https://book.cakephp.org/3.0/en/development/testing.html

---
+++ tests/Fixture/ArticlesFixture.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+class ArticlesFixture extends TestFixture {
+
+    // Optional. Set this property to load fixtures to a different test datasource
+    public $connection = 'test';
+    
+    //define table database
+    /*public $fields = [
+        'id' => ['type' => 'integer'],
+        'title' => ['type' => 'string', 'length' => 255, 'null' => false],
+        'body' => 'text',
+        'published' => ['type' => 'integer', 'default' => '0', 'null' => false],
+        'created' => 'datetime',
+        'modified' => 'datetime',
+        '_constraints' => [
+            'primary' => ['type' => 'primary', 'columns' => ['id']]
+        ]
+    ];*/
+    
+    //alternatively we can define the table fields by importing it from database
+    //public $import = ['table' => 'articles']; //this will find table 'articles' literally in database
+    public $import = ['model' => 'Articles']; //this will find 'Articles' literally in model
+
+    //this style has weakness which is you cannot set the created date using date('Y-m-d')
+    /* public $records = [
+      [
+      'title' => 'First Article',
+      'body' => 'First Article Body',
+      'published' => '1',
+      'created' => '2007-03-18 10:39:23',
+      'modified' => '2007-03-18 10:41:31'
+      ]
+      ]; */
+
+    //so we use init(...)
+    public function init() {
+        $this->records = [
+            [
+                'title' => 'First Article',
+                'body' => 'First Article Body',
+                'published' => '1',
+                'created' => '2007-03-18 10:39:23',
+                'modified' => '2007-03-18 10:41:31'
+            ],
+            [
+                'title' => 'Second Article',
+                'body' => 'Second Article Body',
+                'published' => '1',
+                'created' => '2007-03-18 10:41:23',
+                'modified' => '2007-03-18 10:43:31'
+            ],
+            [
+                'title' => 'Third Article',
+                'body' => 'Third Article Body',
+                'published' => '1',
+                'created' => '2007-03-18 10:43:23',
+                'modified' => '2007-03-18 10:45:31'
+            ]
+        ];
+        parent::init();
+    }
+
+}
---
+++ tests/TestCase/View/Helper/ProgressHelperTest.php
@@ -0,0 +1,43 @@
+<?php
+namespace App\Test\TestCase\View\Helper;
+
+use App\View\Helper\ProgressHelper;
+use Cake\TestSuite\TestCase;
+use Cake\View\View;
+
+class ProgressHelperTest extends TestCase
+{
+    public $autoFixtures = false;
+    
+    public $fixtures = [
+        'app.articles', //will take from \tests\Fixture\ArticlesFixture.php
+    ];
+    
+    public function setUp()
+    {
+        parent::setUp();
+        $View = new View();
+        $this->Progress = new ProgressHelper($View);
+    }
+    
+    public function testLoadFixtureOntheFly()
+    {
+        $this->loadFixtures('Articles');
+    }
+    
+    public function testHello()
+    {
+        //this test will fail
+        //$this->assertContains('HELLO', 'HI');
+    }
+
+    public function testBar()
+    {
+        $result = $this->Progress->bar(90);
+        $this->assertContains('width: 90%', $result);
+        $this->assertContains('progress-bar', $result);
+
+        $result = $this->Progress->bar(33.3333333);
+        $this->assertContains('width: 33%', $result);
+    }
+}
